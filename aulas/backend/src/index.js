2e282ef00dfd75d0cccb7f5d1ea00f02640d3815
Nijoane <nicajoane@outlook.com>
2020-09-23 23:17:12 -0300

    next-level-week

---
+++ aulas/backend/src/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes'); 
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+/**
+     * Rota / Recurso
+     */
+
+ /**
+     *          Métodos HTTP:
+     * 
+     * GET: Buscar/listar uma informação do back-end
+     * POST: Criar uma informação no back-end
+     * PUT: Alterar uma informação no back-end
+     * DELETE: Deletar uma informação no back-end
+     */ 
+
+/**
+     *        Tipos de parâmentros: 
+     * 
+     * Query Params: Parâmetros nomeados enviados na rota após "?" (filtros/paginação)
+     * Route Params: Parâmetros utilizados para identificar recursos
+     * Request Body: Corpo da requisição, ultilizado para criar ou alterar recursos
+     */ 
+
+ /**
+     *          Bancos de Dados: relacionais e não relacionais:
+     * 
+     * SQL: MySQL, SQLite PostgreSQL, Oracle, Microsoft SQL Server
+     * NOSQL: MongoDB, CouchDB etc
+     */
+
+/**
+     * Driver: SELECT * FROM users
+     * Query Builder: table('users').select('*').where()
+     */
+
+
+app.listen(3333);
